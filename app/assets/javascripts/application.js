3bc011b1c60ddd0e8ea81653116b96f5372c4434
Michelle <michelle@Macintosh-10.local>
2016-06-05 17:46:44 -0400

    adds hardcoded token

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -92,11 +92,12 @@ myApp.controller("ArtworksController", ["$scope", "$http", function( $scope, $ht
 
   // search for an artist by search term
   $scope.search = function (searchTerm) {
-    console.log("Search Term: " + searchTerm)
+    console.log("Search Term: " + searchTerm);
     $scope.artworks = [];
 
     $http.get('/token').then(function(response){
-      $scope.xapp = response.data.token
+      $scope.xapp = response.data.token;
+      $scope.xapp = "JvTPWe4WsQO-xqX6Bts49mdCflE7ZOlCwb9EQ_7NlXGTePMdR1oNz7f4ahM7K9RVjN_nOTdPrPiF1h-jzXxPUQYd0mpNteR7LjGfJFcsarWAVN7Qjm4R93NYVIx-iddGRWWsnk5UplN7g1H0xPZNkYFI8h0D9fpy1grf-5Ox0LQlIuaPOs4sIP7TQ6-RjjVuaD_E0ROnYOc7pFFtB6MQLZGZvUl6AIvyrnyjcrhh_JY=";
       console.log($scope.xapp);
       var searchReq = {
         method: 'GET',
